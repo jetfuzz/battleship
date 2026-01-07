b5d6c498ccf7c755e3c52efd2a5b34741e9ca527
jetfuzz <1jsf23@gmail.com>
2026-01-07 10:58:19 -0600

    Implement Ship class with tests

---
+++ src/ship.js
@@ -0,0 +1,19 @@
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.hitCount = 0;
+    }
+
+    hit() {
+        this.hitCount++
+    }
+
+    isSunk() {
+        if (this.hitCount >= this.length) {
+            return true;
+        }
+        return false;
+    }
+}
+
+export default Ship
---
+++ tests/ship.test.js
@@ -0,0 +1,27 @@
+import Ship from '../src/ship.js';
+
+describe('Ship', () => {
+  test('has a length', () => {
+    const ship = new Ship(3);
+    expect(ship.length).toBe(3);
+  });
+
+  test('starts with zero hits', () => {
+    const ship = new Ship(3);
+    expect(ship.hitCount).toBe(0);
+  });
+
+  test('hit method increases hitCount', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    expect(ship.hitCount).toBeGreaterThan(0);
+  });
+
+  test('sinks when hitCount is equal to length', () => {
+    const ship = new Ship(3);
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+  });
+});
