5b40fb44bae7ae160102a3271e5546a494f50422
jetfuzz <1jsf23@gmail.com>
2026-01-25 12:44:43 -0600

    add Node globals to ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,4 +8,10 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    files: ["**/*.config.js"],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
 ]);
