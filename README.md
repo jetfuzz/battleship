dd10a0d5b4c1d6ba8249b973d2377d1ef5d89d38
jetfuzz <1jsf23@gmail.com>
2026-02-23 16:21:03 -0600

    Add README and fix sunk ship marker for Windows

---
+++ README.md
@@ -0,0 +1,13 @@
+# Battleship
+
+A web-based Battleship game built with Test-Driven Development.
+
+[Live Demo](https://jetfuzz.github.io/battleship/)
+
+## Features
+- Play against computer AI
+- Random ship placement
+- Visual feedback for hits, misses, and sunk ships
+
+## Built With
+JavaScript • Jest • Webpack • HTML/CSS
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+      favicon: "./src/battleship-svgrepo-com.png"
     }),
   ],
   module: {
@@ -23,6 +24,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
